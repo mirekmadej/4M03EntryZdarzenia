67dcb37b905ceb9f3164d44d86eb752788146420
Mirosław Madej <M.Madej@zsp3zamosc.pl>
2024-09-16 12:21:03 +0200

    Dodaj pliki projektów.

---
+++ 4M03EntryZdarzenia/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace _4M03EntryZdarzenia
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ 4M03EntryZdarzenia/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace _4M03EntryZdarzenia
+{
+    public partial class MainPage : ContentPage
+    {
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void onEntry1Completed(object sender, EventArgs e)
+        {
+            lblE1.Text = entE1.Text;
+        }
+        private void onEntry2TextChanged(object sender, EventArgs e)
+        {
+            lblE2.Text = entE2.Text;
+        }
+
+
+    }
+
+}
